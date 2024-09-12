5d7c4fc8e06dac73a7672f751675c7d50af6adaa
Dasun Sri Nethmal <166989561+nethmalds@users.noreply.github.com>
2024-09-12 14:38:06 +0530

    feat(home-page): vision & mission section
    * feat: adding components and initial changes
    
    * feat: adding components and initial changes
    
    * feat(images): add new logos and visual assets for IEEE NSBM sections
    - Added IEEE NSBM CS Logo (Color and Color with White Text)
    - Added IEEE NSBM SB Logo (Blue variant)
    - Added Mission and Vision SVG files
    - Added WIE Logo (Color variant)
    
    feat(ui): implement ImageSwitcher component for dynamic image display
    - Created ImageSwitcher.tsx component
    
    fix(styles): update global styles and homepage layout
    - Modified globals.css for style improvements
    - Updated page.tsx for layout adjustments
    
    * fix(styles): update global styles and layout adjustments
    - Modified globals.css for improved styling
    - Updated page.tsx to refine layout and structure
    - Adjusted ImageSwitcher.tsx component for better image handling
    
    * feat: update logo and add VMSection component
    - Replaced old IEEE NSBM SB logo with new colored version
    - Updated `page.tsx` and `ImageSwitcher.tsx` to reflect new changes
    - Added new `VMSection.tsx` component
    
    * refactor: update layout and functionality in page and components
    - Updated `page.tsx` for improved layout and structure
    - Modified `ImageSwitcher.tsx` for enhanced image handling
    - Adjusted `VMSection.tsx` for better content display
    
    * chore(assets): renames to comply with kebab case
    
    * chore: update global styles and rename components for consistency
    - Updated `globals.css` with new styles
    - Modified `page.tsx` for content/layout adjustments
    - Renamed `ImageSwitcher.tsx` to `image-switcher.tsx` for consistency
    - Renamed `VMSection.tsx` to `vm-section.tsx` for consistency
    - Updated `tailwind.config.ts` for new configurations
    
    * chore(husky): update pre-push hook
    style(globals.css): update global styles
    feat(image-switcher): enhance image switching functionality
    feat(vm-section): improve VM section component
    
    * Merge branch 'release/development' of https://github.com/IEEE-Student-Branch-NSBM/IEEE-Student-Branch-NSBM.github.io into feat/VMSection
    
    ---------
    
    Co-authored-by: Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import BranchCounsellors from "@/components/index/branch-counsellors";
+import React from "react";
 
+import BranchCounsellors from "@/components/index/branch-counsellors";
 import EventCard from "@/components/index/event-card";
+import VisionMissionSection from "@/components/vm-section";
 
 export default function Home() {
   return (
     <div>
+      <VisionMissionSection />
       <BranchCounsellors />
       <EventCard />
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
   darkMode: ["class"],
   content: [
