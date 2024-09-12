fdee207cb7a6e5822219f8a1ab4f58d75d098ea9
Navindu Dissanayake <121451930+Navindu-Ashen@users.noreply.github.com>
2024-09-12 13:31:01 +0530

    feat(home-page): implements branch counsellers section (#24)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import BranchCounsellors from "@/components/index/branch-counsellors";
+
 import EventCard from "@/components/index/event-card";
 
 export default function Home() {
   return (
     <div>
+      <BranchCounsellors />
       <EventCard />
     </div>
   );
