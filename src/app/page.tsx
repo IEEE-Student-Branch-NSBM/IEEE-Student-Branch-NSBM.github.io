fa829621f32051915d1417832265dedbac93baab
Kumuditha Ranasinghe <ranasinghe24125@gmail.com>
2024-09-13 12:53:38 +0530

    feat(feat/contact-us): impliment contact us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Contactus from "@/components/contactus";
 import BranchCounsellors from "@/components/index/branch-counsellors";
 import EventCard from "@/components/index/event-card";
 import VisionMissionSection from "@/components/vm-section";
