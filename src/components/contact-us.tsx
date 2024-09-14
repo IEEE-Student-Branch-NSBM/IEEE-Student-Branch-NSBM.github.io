842cf20b91461d7cda14691b496ae01a7b73dc2a
Kumuditha Ranasinghe <118108730+KumudithaRanasinghe@users.noreply.github.com>
2024-09-14 16:40:19 +0530

    feat(contact-page): impliments contact us page (#27)
    * fix(home-page/events-setion-responsive-changes): fix mobile responsiveness
    
    * feat(feat/contact-us): impliment contact us page
    
    * fix(feat/contact-us): fix mobile responsiveness
    
    * chore(contact-page): minor fixes & quality changes
    
    ---------
    
    Co-authored-by: Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>

---
+++ src/components/contact-us.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+const formSchema = z.object({
+  firstName: z.string().min(1, { message: "First name is required" }),
+  secondName: z.string().min(1, { message: "Second name is required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  message: z.string().min(1, { message: "Message is required" }),
+});
+
+const ContactUs = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      secondName: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="container mx-auto p-4 lg:p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-5 md:mx-8 lg:mx-8  ">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">
+              Our friendly team wants to hear from you
+            </CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <div className="grid grid-cols-2 gap-4">
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="First Name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="secondName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Second Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Second Name" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="message"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Message</FormLabel>
+                      <FormControl>
+                        <Textarea placeholder="Type Message" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors  "
+                  type="submit"
+                >
+                  SEND MESSAGE
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+
+        <div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.575796315297!2d80.03899797406531!3d6.821334419632553!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2523b05555555%3A0x546c34cd99f6f488!2sNSBM%20Green%20University!5e0!3m2!1sen!2slk!4v1726211085363!5m2!1sen!2slk"
+            width="100%"
+            height="450"
+            className="rounded"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
