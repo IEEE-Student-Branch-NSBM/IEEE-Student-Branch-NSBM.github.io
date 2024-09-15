3021aaf5c06137438d872c56ea79a1e28ed3527f
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2024-09-15 15:49:18 +0530

    fix: clean up & minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import BranchCounsellors from "@/components/index/branch-counsellors";
 import EventCard from "@/components/index/event-card";
 import ExecutiveCommittee from "@/components/index/executive-committee";
-import Fellowship from "@/components/shared/fellowship";
+import Fellowship from "@/components/index/fellowship";
 import VisionMissionSection from "@/components/vm-section";
 
 export default function Home() {
