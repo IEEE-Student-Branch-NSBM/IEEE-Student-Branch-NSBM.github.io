74de4a94e8a20253b2637e95f0803c2598552289
Yasiru Dharmathilaka <yasiru.dharmathilaka@gmail.com>
2024-10-04 02:07:31 +0530

    chore: minor clean ups, optimizations, better file grouping, & naming changes (#41)

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,10 +1,11 @@
-import AboutCardTitle from "@/components/about-card-title";
-import AboutInfoCard from "@/components/about-info-card";
+import AboutCardTitle from "@/components/about-us/about-card-title";
+import AboutInfoCard from "@/components/about-us/about-info-card";
 
 export default function About() {
   return (
     <div>
       <AboutCardTitle data="#00629b[IEEE] NSBM-IEEE STUDENT BRANCH OF NSBM" />
+
       <AboutInfoCard
         data='IEEE Computer Society (sometimes abbreviated as the Computer Society or CS) is a professional
       society of the Institute of Electrical and Electronics Engineers (IEEE). Its purpose and scope are "to advance the theory,
@@ -15,14 +16,18 @@ export default function About() {
       in educational activities at all levels of the profession, including distance learning, accreditation of higher education programs
       in computer science, and professional certification in software engineering.|https://nsbmieee.org/_nuxt/image/cd440c.webp'
       />
+
       <AboutCardTitle data="#702f8a[WIE] Affinity Group-IEEE WOMEN IN ENGINEERING AFFINITY GROUP OF NSBM" />
+
       <AboutInfoCard
         data="IEEE Women in Engineering (WIE) is a global platform of IEEE members and volunteers dedicated to advancing
       women engineers and scientists and empowering young girls throughout the world to pursue academic interests in engineering
       and scientific professions. Women in Engineering Affinity Group of NSBM (WIE NSBM) is one of the student branch chapters
       of the global network which uplifts the women in leadership roles in industry as well as international platforms.|https://nsbmieee.org/_nuxt/image/0eeb4d.webp"
       />
+
       <AboutCardTitle data="#ffa300[Computer Society] NSBM-IEEE COMPUTER SOCIETY STUDENT BRANCH CHAPTER OF NSBM" />
+
       <AboutInfoCard
         data='IEEE Computer Society (sometimes abbreviated as the Computer Society or CS) is a professional society of the Institute
       of Electrical and Electronics Engineers (IEEE). Its purpose and scope are "to advance the theory, practice, and application of computer
